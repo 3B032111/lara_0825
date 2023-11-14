6e040591f7da9e02a8948cf154583dac2c3adf8a
3B032111 <3b032111@gm.student.ncut.edu.tw>
2023-11-14 10:50:43 +0800

    git practice 6-1

--- routes/web.php
+++ routes/web.php
@@ -16,62 +16,43 @@ use App\Models\Comment;
 
 Route::get('/', function () {
 
-    /*
-    Post::create([
+
+   /* Post::create([
         'title'=>'created title',
         'content'=>'created content',
-    ]);
-    */
+    ]);*/
 
-    /*
-    $post = new Post();
+    /* $post = new Post();
     $post->title = "test title";
     $post->content = "test content";
-    $post->save();
-    */
+    $post->save();    */
 
-    /*
-    $post = Post::find(1);
+
+
+   /* $post = Post::find(1);
     echo '標題: '.$post->title.'<br>';
     echo '內容: '.$post->content.'<br>';
-    dd($post);
-    */
+    dd($post); */
 
-    /*
-    $posts = Post::all();
+   // $posts = Post::all();
 
     // 每次迴圈依序取出一篇貼文
-    foreach($posts as $post){
+    /* foreach($posts as $post){
         echo '編號: '.$post->id.'<br>';
         echo '標題: '.$post->title.'<br>';
         echo '內容: '.$post->content.'<br>';
         echo '張貼時間: '.$post->created_at.'<br>';
-    }
-    dd($post);
-    */
+    } */
 
-    /*
-    $posts = Post::where('id', '<', 10)->orderBy('id', 'DESC')->get();
-    dd($posts);
-    */
-    /*
-        $post->title = 'saved title';
-        $post->content = 'saved content';
-        $post->save();*/
+    /* dd($post);
+   /*$posts = Post::where('id', '<', 10)->orderBy('id', 'DESC')->get();
+     dd($posts);
 
+         $post->title = 'saved title';
+         $post->content = 'saved content';
+         $post->save();*/
 
-        // Post::destroy(3,5,7);
-    //$allPosts = Post::all();
-    //dd($allPosts);
-
-   // $featurePosts = Post::where('is_feature', 1)->get();
-    //dd($featurePosts);
-
-   // $fourthPost = Post::find(4);
-   // dd($fourthPost);
-
-    //$lastPost = Post::orderBy('id','DESC')->first();
-   // dd($lastPost);
-
+    $allPosts=Post::all();
+    dd($allPosts);
 
 });
